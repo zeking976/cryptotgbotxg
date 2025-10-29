a022c306f896a8036ff364fc3e1a2fc5b01d283d
zeking976 <what49565@gmail.com>
2025-10-29 06:55:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from dotenv import load_dotenv
 from token_fetcher import TokenFetcher
 from filters import passes_filters
 from telegram_sender import TelegramSender
+
 load_dotenv('t.env')
-BOT_TOKEN    = os.getenv("TELEGRAM_BOT_TOKEN")
-CHANNEL_ID   = os.getenv("TARGET_CHANNEL_ID")
-BITQUERY_KEY = os.getenv("BITQUERY_API_KEY")
-RPC          = os.getenv("RPC_ENDPOINT", "https://api.mainnet-beta.solana.com")
-if not all([BOT_TOKEN, CHANNEL_ID, BITQUERY_KEY]):
+
+BOT_TOKEN   = os.getenv("TELEGRAM_BOT_TOKEN")
+CHANNEL_ID  = os.getenv("TARGET_CHANNEL_ID")
+MORALIS_KEY = os.getenv("MORALIS_API_KEY")
+RPC         = os.getenv("RPC_ENDPOINT", "https://api.mainnet-beta.solana.com")
+
+if not all([BOT_TOKEN, CHANNEL_ID, MORALIS_KEY]):
     raise SystemExit("Missing env vars")
-print("Moralis Pump Bot starting...🚀")
+
+print("Moralis Pump Bot starting🚀...")
 sender  = TelegramSender(BOT_TOKEN, CHANNEL_ID)
-fetcher = TokenFetcher(BITQUERY_KEY)
+fetcher = TokenFetcher(MORALIS_KEY)
 seen    = set()
+
 async def loop():
     while True:
         new_tokens = fetcher.get_new_pump_tokens()
         trend_tokens = fetcher.get_trending_pump_tokens()
         all_tokens = new_tokens + trend_tokens
-        print(f"Processing {len(all_tokens)} Pump candidates...")
+
+        print(f"Processing {len(all_tokens)} candidates📊...")
         for t in all_tokens:
             mint = t["mint"]
             if mint in seen: continue
@@ -27,8 +33,9 @@ async def loop():
                 await sender.send_token(mint)
                 seen.add(mint)
             else:
-                print(f"❌  Skipped {mint}")
-        print("Sleeping 12s...")
+                print(f"❌Skipped {mint}")
+        print("Sleeping 12s🔃...")
         await asyncio.sleep(12)
+
 if __name__ == "__main__":
     asyncio.run(loop())
